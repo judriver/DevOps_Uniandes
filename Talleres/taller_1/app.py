d9e4fed3dc19e7c65c10115c2193d643fb5b5870
Juan David Rivera Builes <judriver@bancolombia.com.co>
2024-09-26 12:02:37 -0500

    Nueva funcion para cargar datos

--- Talleres/taller_1/app.py
+++ Talleres/taller_1/app.py
@@ -22,6 +22,10 @@ app.config.suppress_callback_exceptions = True
 # Load data from csv
 def load_data():
     # To do: Completar la función 
+    data=pd.read_csv("datos_energia.csv")
+    data["time"]=pd.to_datetime(data["time"])
+    df = data.set_index('time')
+    return df
     
 
 # Cargar datos
